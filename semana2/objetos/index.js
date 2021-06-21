6bdb964f660d89570de5acb65c1c45ff9bde026b
MatheusViniciusVianaVieira <matheusviana613@gmail.com>
2021-06-21 16:34:53 -0300

    aula07

---
+++ semana2/objetos/index.js
@@ -0,0 +1,28 @@
+//interpretação de codigo
+
+//1 exercicio 
+// Matheus Nachtergaele
+//Virginia Cavendish
+//{canal: "Globo", horario: "14h"}
+
+
+// 2 exercicio
+// A
+//index.js:19 {nome: "Juca", idade: 3, raca: "SRD"}
+//index.js:20 {nome: "Juba", idade: 3, raca: "SRD"}
+//index.js:21 {nome: "Jubo", idade: 3, raca: "SRD"}
+
+//B o sintaxe faz uma copia do objeto 
+
+
+//3 exercicio 
+//a
+// false
+
+//b
+// no backender estar false, ou seja o retorno da variavel seria false.
+
+
+//Exercicio de escrita de codigo
+
+
